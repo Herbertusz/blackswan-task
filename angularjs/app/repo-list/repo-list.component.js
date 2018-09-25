d00a87f4c91ac5ede2e90c2221d39619705b9bb7
Horváth Norbert <horv.norbert@gmail.com>
2018-09-25 08:43:02 +0200

    AngularJS task - repository list

--- angularjs/app/repo-list/repo-list.component.js
+++ angularjs/app/repo-list/repo-list.component.js
@@ -4,21 +4,38 @@
 
 angular.module('App').component('repoList', {
     templateUrl : 'app/repo-list/repo-list.template.html',
-    controller : function($http){
+    controller : function($http, $routeParams, $location){
         this.orderProp = 'name';
-        this.search = function(text){
-            if (typeof text === 'undefined' || text.length < 1){
-                alert('NEM!');
-                return;
+        this.search = function(searchText, keyEvent = null){
+            const condition = (
+                (!keyEvent || (keyEvent && keyEvent.key === 'Enter')) &&
+                typeof searchText !== 'undefined' && searchText.length > 0
+            );
+            if (condition){
+                $http.get(`https://api.github.com/search/repositories?q=${searchText}`)
+                    .then(resp => {
+                        if (resp.data.total_count > 0){
+                            const displayProperties = [
+                                'id', 'name', 'full_name', 'html_url', 'stargazers_count',
+                                'forks_count', 'open_issues_count'
+                            ];
+                            this.repositories = resp.data.items.map(
+                                repo => displayProperties.reduce(
+                                    (obj, prop) => ({...obj, [prop] : repo[prop]}), {}
+                                )
+                            );
+                        }
+                        else {
+                            this.repositories = [];
+                        }
+                        $location.update_path(`/repositories/${searchText}`);
+                    }).catch(error => {
+                        console.log(error);
+                    });
             }
-            $http.get('app/data/phones.json')
-                .then(resp => {
-                    this.repositories = resp.data.filter(repo =>
-                        repo.name.toLowerCase().indexOf(text.toLowerCase()) > -1
-                    );
-                }).catch(error => {
-                    console.log(error);
-                });
         };
+        if (typeof $routeParams.searchText !== 'undefined'){
+            this.search($routeParams.searchText);
+        }
     }
 });
